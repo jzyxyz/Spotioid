b240a2099033a0be31b1db14b1a3eae57c8cc9a8
Zhongyuan Jin <nanomalloc@gmail.com>
2019-09-23 23:17:50 +0300

    feature rank index

--- src/Main.js
+++ src/Main.js
@@ -6,8 +6,31 @@ import InfoBlock from './InfoBlock'
 import dataIndex from './dataIndex'
 import Map from './Map'
 import InputSuggest from './InputSugguest'
-import { trim } from 'lodash'
+import { trim, orderBy } from 'lodash'
 import COUNTRY_NAMES from './COUNTRY_LIST'
+
+let features = Object.values(dataIndex)
+  .filter(el => el.name)
+  .map(({ name, features }) => ({
+    name,
+    features,
+  }))
+const f_Keys = Object.keys(features[0].features)
+const featureRankIndex = {}
+features = f_Keys.forEach(fk => {
+  featureRankIndex[fk] = orderBy(features, [el => el.features[fk]], 'desc')
+})
+console.log(featureRankIndex)
+const chartData = Object.values(dataIndex)
+chartData.forEach(p => {
+  f_Keys.forEach(k => {
+    p.features[k] = {
+      value: p.features[k],
+      rank: featureRankIndex[k].findIndex(d => d.name === p.name) + 1,
+    }
+  })
+})
+console.log(chartData.map(({ name, features }) => ({ name, features })))
 
 const Main = () => {
   const inputRef = useRef(null)
@@ -38,7 +61,7 @@ const Main = () => {
       // setAllSuggestions(choices)
       if (event.keyCode === 13) {
         console.log(555, choices)
-        setSelected(choices[0])
+        setSelected([choices[0]])
         setInput(choices[0])
         event.target.value = choices[0]
         setSuggestions([])
