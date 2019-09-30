948db44bc148f5659a7fd7bfdc0e75fb9b078020
Envl <yyaomingm@gmail.com>
2019-09-30 16:55:52 +0200

    footer prep

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer-p'>
+        <div className='footer-name'></div>
+        <div className='footer-contact'></div>
+      </div>
+      <div className='footer-p'>
+        <div className='footer-name'></div>
+        <div className='footer-contact'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
