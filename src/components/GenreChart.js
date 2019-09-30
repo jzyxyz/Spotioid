c1cd5b44d99d497b72eb63622ede05da556f237b
Zhongyuan Jin <nanomalloc@gmail.com>
2019-09-30 15:16:49 +0300

    clean up and change color

--- src/components/GenreChart.js
+++ src/components/GenreChart.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import BubbleChart from './BubbleChart'
 
-const GenreChart = ({data, className}) => {
-  console.log(data)
-  const bubbleClick = label => {
-    console.log('Custom bubble click func')
-  }
+const GenreChart = ({ data, className }) => {
+  // console.log(data)
 
   return (
     <BubbleChart
       className={className}
       graph={{
-        zoom: 0.7,
-        offsetX: -0.05,
-        offsetY: -0.01,
+        zoom: 0.5,
       }}
-      heigth={400}
-      width={400}
-      padding={0} // optional value, number that set the padding between bubbles
+      heigth={800}
+      width={800}
+      padding={5} // optional value, number that set the padding between bubbles
       showLegend={false} // optional value, pass false to disable the legend.
-      bubbleClickFunc={bubbleClick}
       data={data}
     />
   )
