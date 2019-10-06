533ba75c4ec04af8a5f27e88e2af7a30c6162bd8
Zhongyuan Jin <nanomalloc@gmail.com>
2019-10-06 15:46:32 +0300

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Report from './Report'
 import MapPage from './MapPage'
+import {
+  faInfo,
+  faBookOpen,
+  faCopyright,
+  faShare,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Main = () => {
   return (
@@ -10,15 +17,50 @@ const Main = () => {
         <Route path='/report' component={Report} />
         <Route path='/' component={MapPage} />
       </Switch>
-      {/* 
+
       <div className='foot'>
-        <div className='foot-info'>
-          <div>About the Project </div>
-          <Link to='/report'>
-            <div>Read weekly Report</div>
+        <div className='foot-icon-group'>
+          <Link to='/about'>
+            <div className='svg-container'>
+              <FontAwesomeIcon icon={faInfo} />
+            </div>
           </Link>
+          <Link to='/report'>
+            <div className='svg-container'>
+              <FontAwesomeIcon icon={faBookOpen} />
+            </div>
+          </Link>
+          <div className='svg-container'>
+            <FontAwesomeIcon icon={faCopyright} />
+          </div>
+          <div
+            className='svg-container'
+            onClick={() => {
+              navigator.permissions
+                .query({ name: 'clipboard-write' })
+                .then(result => {
+                  if (result.state == 'granted' || result.state == 'prompt') {
+                    /* write to the clipboard now */
+                    navigator.clipboard
+                      .writeText(
+                        `Checkout this interesting website ${window.location}`,
+                      )
+                      .then(
+                        function() {
+                          /* clipboard successfully set */
+                        },
+                        function() {
+                          /* clipboard write failed */
+                        },
+                      )
+                  }
+                })
+            }}
+          >
+            <FontAwesomeIcon icon={faShare} />
+          </div>
         </div>
-      </div> */}
+      </div>
     </Router>
   )
 }
